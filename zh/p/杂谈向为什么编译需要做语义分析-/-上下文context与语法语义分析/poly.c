dee4e8b3c6d0c4f6970838444f98a4577144bf7b
fyshxfish <fyshx121120@gmail.com>
2025-02-17 20:51:21 +0800

    fix

---
+++ zh/p/杂谈向为什么编译需要做语义分析-/-上下文context与语法语义分析/poly.c
@@ -0,0 +1,22 @@
+#define a /*
+echo -e "\033[34mHello, world! from echo\033[0m";// &> /dev/null;
+x=5; 
+if (($x))
+// 2> /dev/null; then
+return 0;
+// 2> /dev/null; fi
+#define e ?>
+#define b */
+#include <stdio.h>
+#define main() int main(void)
+#define printf printf(
+#define true )
+#define function
+function main()
+{
+    printf "\033[31mHello, world!\033[0m\n"true/* 2> /dev/null | grep -v true*/;
+    return 0;
+}
+#define c /*
+main
+#*/
